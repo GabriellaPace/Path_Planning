f846dd53457c851ab9db7698694103929a736e2e
Gabriella <pcgabriell@virgilio.it>
2022-04-02 17:47:30 +0200

    feat: simple queue works

--- Path_Planning/MODLite.cpp
+++ Path_Planning/MODLite.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <limits>		// for min/max
 //#include <optional>   // -> might be useful for key & predecessor
+#include <queue>
+//#include <functional>
+//#include <vector>
 
 
-// Node = state in Koeing
-class Node {
+class Node {			// Node = state in Koeing
 public:
 	int X;
 	int Y;
@@ -22,24 +24,109 @@ public:
 	void* predecessor;
 
 	// constructor:
-	Entity(int x, int y, bool isStartNode, bool isGoalNode) {
+	Node() {}
+
+	Node(int x, int y, bool isStartNode, bool isGoalNode) {
 		X = x;
 		Y = y;
 
 		isStart = isStartNode;
 		isGoal = isGoalNode;
 
+		if (isGoal) {
+			g = 0.0f;
+			rhs = 0.0f;
+		}
+		else {
+			g = std::numeric_limits<float>::infinity();
+			rhs = std::numeric_limits<float>::infinity();
+		}
+	
 		key.first = -1;
 		key.second = -1;
 		predecessor = nullptr;
+	}
 
-		g = std::numeric_limits<float>::infinity();
-		rhs = std::numeric_limits<float>::infinity();
+
+	// to sort the queue  ////////////////////////////////////////////////////////////
+	//struct CompareKey {
+	bool operator > (const Node &N2) const {
+		//bool result;
+		if (key.first > N2.key.first)
+			return true;			
+		else if (key.first < N2.key.first) 
+			return false; 			
+		else { // key.first == N2.key.first
+			if (key.second > N2.key.second)
+				return true;
+			else // (key.second <= N2.key.second)
+				return false; 
+		}
+		//return result; //return key.second > N2.key.second;
+	}
+	//};
+
+	bool operator < (const Node &N2) const {
+		if (key.first < N2.key.first)
+			return true;
+		else if (key.first > N2.key.first)
+			return false;
+		else { // key.first == N2.key.first
+			if (key.second < N2.key.second)
+				return true;
+			else // (key.second >= N2.key.second)
+				return false;
+		}
+	}
+
+	bool operator == (const Node &N2) const {
+		if (key.first == N2.key.first  &&  key.second == N2.key.second)
+			return true;
+		else
+			return false;
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////
+	void print_Node() {
+		std::cout << key.first << " , " << key.second << std::endl;
 	}
 };
 
 
 
+template<typename T>
+void print_queue(T& q) {
+	Node tmp;
+	while (!q.empty()) {
+		tmp = q.top();
+		tmp.print_Node();
+		q.pop();
+	}
+}
+
+
 int main() {
+	//std::priority_queue<Node, std::vector<Node>, CompareKey >  queue;
+	std::priority_queue<Node, std::vector<Node>, std::greater<Node> >  queue;
+
+	//////////////////////////////////
+	Node n1(0, 0, true, false);
+	Node n2(1, 0, false, false);
+	Node n3(2, 0, false, true);
+
+	n1.key.first = 0.0f;
+	n1.key.second = 0.0f;
+	queue.push(n1);
+
+	n2.key.first = 1.0f;
+	n2.key.second = 4.0f;
+	queue.push(n2);
+
+	n3.key.first = 2.0f;
+	n3.key.second = 0.0f;
+	queue.push(n3);
+	//////////////////////////////////
+
+	print_queue(queue);
 	std::cin.get();
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Path_Planning
 Multi-objective path planning for Mars Rover
+
+------ NOTES ------
+std::priority_queue is a "container adaptor". By default std::vector is the container used inside.
